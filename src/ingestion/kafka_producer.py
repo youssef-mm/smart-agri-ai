f616c7a2a647671dde2d1bf5e87fd5d8f5464efb
Youssef Mohamed <s24510620eg@std.aou.edu.eg>
2026-08-05 23:34:21 +0300

    Refactor Kafka producer to use environment variable for server

--- src/ingestion/kafka_producer.py
+++ src/ingestion/kafka_producer.py
@@ -1,29 +1,30 @@
+import os
+import time
+import json
+from pathlib import Path
 from kafka import KafkaProducer
 import pandas as pd
-import json
-import time
-from pathlib import Path
 
 TOPIC_NAME = "crop_yield_topic"
 BATCH_SIZE = 1000
+
+# بيقرأ kafka:9092 لو جوه container أو localhost:9092 لو شغال محلياً
+BOOTSTRAP_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVER", "localhost:9092")
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 DATA_FILE = BASE_DIR / "data" / "crop_yield.csv"
 
 try:
     producer = KafkaProducer(
-        bootstrap_servers="localhost:9092",
+        bootstrap_servers=BOOTSTRAP_SERVER,
         value_serializer=lambda v: json.dumps(v).encode("utf-8")
     )
 
     chunks = pd.read_csv(DATA_FILE, chunksize=BATCH_SIZE)
-
     batch_number = 1
 
     for chunk in chunks:
-
         records = chunk.to_dict(orient="records")
-
         for record in records:
             producer.send(
                 TOPIC_NAME,
@@ -31,15 +32,11 @@ try:
             )
 
         producer.flush()
-
         print(f"✅ Batch {batch_number} sent ({len(records)} records)")
-
         batch_number += 1
-
         time.sleep(0.5)
 
     producer.close()
-
     print("🎉 All batches sent successfully!")
 
 except Exception as e:
